928f60f8e136ec2eac283dfe7dab5231eff639c2
nwalter <nwalter@suse.de>
2014-09-03 14:09:23 +0200

    add test infrastructure

---
+++ memo.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+
+class Memo
+  def run
+    puts 'Hallo'
+  end
+end
+
--- spec/memo_spec.rb
+++ spec/memo_spec.rb
@@ -1,1 +1,12 @@
 require 'spec_helper'
+
+describe "memo" do
+
+  subject { Memo.new }
+
+  it 'says hello' do
+    expect(subject).to receive(:puts).with('Hallo')
+    subject.run
+  end
+
+end
