207916db42bd76e7550d3ca5188d7e409b66d92e
nwalter <nwalter@suse.de>
2014-09-29 12:25:29 +0200

    store function: write input to file file_name function: generates file name

--- lib/memo.rb
+++ lib/memo.rb
@@ -1,6 +1,24 @@
 class Memo
-  def run
-    puts 'Hallo'
+
+  PREFIX = 'memo'
+  PATH = './'
+
+  def store(input)
+    last_memo = Dir.glob("#{PREFIX}_*").sort.last
+    @number = 1
+    if last_memo
+      @number = last_memo.split('_').last.to_i + 1
+    end
+    File.open(file_name, 'a') do |file|
+      file.write(input)
+    end
   end
+
+  private
+
+  def file_name
+    @number.to_s.rjust(7, "#{PREFIX}_00")
+  end
+
 end
 
